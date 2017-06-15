91605f78d65e61bda4bd6feccc4eecc8d67739d7
Lacy Rhoades <lacyrhoades@gmail.com>
2017-06-15 08:09:18 -0400

    Initial commit

---
+++ client.js
@@ -0,0 +1,85 @@
+const spawn = require('child_process').spawn;
+
+const io = require('socket.io-client');
+const socket = io('https://picam-dev.glitch.me/pi');
+
+var camera = {settings: {exposure: 0.0, effect: "none", vflip: false}};
+
+socket.on('connect', function(){
+  console.log("Connected to server");
+});
+
+socket.on('settings', function(data, ack){
+  if (data.exposure != null) {
+    camera.settings.exposure = data.exposure
+  }
+  if (data.vflip != null) {
+    camera.settings.vflip = data.vflip;
+  }
+  if (data.effect != null) {
+    camera.settings.effect = data.effect;
+  }
+  if (ack) {
+    ack(camera.settings);
+  }
+});
+
+socket.on('disconnect', function(){
+  console.log("Disconnect from server");
+});
+
+socket.on('snap', function() {
+  var args = [
+      '-ifx',
+      camera.settings.effect,
+      '-t',
+      '1',
+      '-h',
+      '480',
+      '-w',
+      '640',
+      '-ev',
+      camera.settings.exposure * 12,
+      '-br',
+      '55',
+      '-mm',
+      'matrix',
+      '-n',
+      '-o',
+      '-'
+    ];
+
+  if (camera.settings.vflip) {
+    args.push('-vf');
+  }
+
+  var child = spawn(
+    'raspistill', args
+  );
+
+  console.log(args);
+
+  var stdout = Buffer.from("");
+  child.on('error', function(err) {
+    console.log("Error running raspistill");
+    console.log(err);
+  });
+  child.stderr.on('data', function(chunk) {
+    console.log("Stderr running raspistill");
+    console.log(chunk);
+  });
+  child.stdout.on('data', function(chunk) {
+    stdout = Buffer.concat([stdout, Buffer.from(chunk)]);
+  });
+  child.on('close', function() {
+    if (stdout != null) {
+      socket.emit('image', stdout);
+    }
+  });
+});
+
+setInterval(function() {
+  console.log("Exposure: " + camera.settings.exposure);
+  console.log("Vflip: " + camera.settings.vflip);
+  console.log("Effect: " + camera.settings.effect);
+}, 500);
